267ea7307dd58f9e1e5ece6b365956a2e845c542
Cemal Yilmaz <Cemal.Yilmaz@telematik-zentrum.de>
2026-07-29 21:39:26 +0200

    Added phase type. Fixed KOZ info print would be None if no KOZ active.

--- agent_simulation/visualization.py
+++ agent_simulation/visualization.py
@@ -407,7 +407,7 @@ def plot_actual_attitude(simulation_data: dict):
 
     print("Minimum margin KOZ:", min_margin_koz*180/np.pi, "degrees")
     print("Count KOZ violations:", cnt_Koz_violations)
-    print("Half angle KOZ:", half_angle_koz*180/np.pi, "degrees")
+    print("Half angle KOZ:", half_angle_koz*180/np.pi, "degrees") if half_angle_koz is not None else print()
     
 
     # Extract rotation axes and angles for all time points
@@ -860,23 +860,25 @@ if __name__ == "__main__":
         Config.Visualization.MAX_NR_KOZ
     ]
 
+    PHASE_TYPE = Config.Visualization.PHASE_TYPE
+
     time_human = datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
 
-    eval_env = create_evaluation_env(INITIAL_STATE, Config.Visualization.MODEL_NAME, Config.Visualization.TIMESTEP)
+    eval_env = create_evaluation_env(PHASE_TYPE, INITIAL_STATE, Config.Visualization.MODEL_NAME, Config.Visualization.TIMESTEP)
     model = load_agent(Config.Visualization.MODEL_NAME, Config.Visualization.TIMESTEP, seed_random = True)
 
     """ Uncomment the lines below to run 1 simulation and plot the results. """
     simulation_data = simulate_episode(model, eval_env, Config.Visualization.MAX_STEPS, Config.Visualization.MODEL_NAME, create_video=Config.Visualization.CREATE_VIDEO)
     #visualize_episode_basilisk(simulation_data, show_basilisk_viz=Config.Visualization.SHOW_BASILISK_VIZ)
     viz_file_path = os.path.join(viz_dir, f"{Config.Visualization.MODEL_NAME}_{INITIAL_STATE}_{time_human}.bin")
-    save_episode_as_viz(viz_file_path, simulation_data, Config.Visualization.SHOW_BASILISK_VIZ, Config.Visualization.VIZARD_EXE_PATH)
-    #plot_actual_attitude(simulation_data)
-    visualize_net_arch(model, eval_env)
+    #save_episode_as_viz(viz_file_path, simulation_data, Config.Visualization.SHOW_BASILISK_VIZ, Config.Visualization.VIZARD_EXE_PATH)
+    plot_actual_attitude(simulation_data)
+    #visualize_net_arch(model, eval_env)
     #plot_for_report(simulation_data, time_end=300)
 
     """ Uncomment the lines below if you have saved evaluation data (from evaluate_agent()) to load all the episodes.
         loaded contains ALL episodes, therefore in loaded[] should be the index of the episode you want to plot.
     """
-    #loaded = load_evaluation_data("rewMod22_phFull_3_ph2_4000000_[90.0, 180.0, 0.0, 0.01, 3000, 15.0, 30.0, 1, 1]_ep[1000]_2026-07-16-16-39-32.npz")
-    #plot_actual_attitude(loaded[708])
+    #loaded = load_evaluation_data("rewMod22_ph1_schedPh1v2_3500000_[90.0, 180.0, 0.0, 0.01, 3000, 15.0, 30.0, 1, 1]_ep[100]_2026-07-29-07-18-34.npz")
+    #plot_actual_attitude(loaded[0])
     #plot_for_report(loaded[0],time_end=300)
