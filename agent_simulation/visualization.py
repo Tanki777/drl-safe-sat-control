8fc15494890cb5603c5e9ea97258f2b02a38cc8f
Cemal Yilmaz <Cemal.Yilmaz@telematik-zentrum.de>
2026-08-16 16:56:59 +0200

    Fixed a plotting error which occured when having no KOZs.

--- agent_simulation/visualization.py
+++ agent_simulation/visualization.py
@@ -224,7 +224,7 @@ def plot_actual_attitude(simulation_data: dict):
         circle_points_array = _generate_keep_out_zone_circles()
         circle_points_array = np.array(circle_points_array)
 
-        ax1.plot(circle_points_array[0][:, 0], circle_points_array[0][:, 1], circle_points_array[0][:, 2], "orange", linewidth=2, label="Keep Out Zone 1")
+        ax1.plot(circle_points_array[0][:, 0], circle_points_array[0][:, 1], circle_points_array[0][:, 2], "orange", linewidth=2, label="Keep Out Zone 1") if len(normal_vector_koz_array) > 0 else None
         ax1.plot(circle_points_array[1][:, 0], circle_points_array[1][:, 1], circle_points_array[1][:, 2], "red", linewidth=2, label="Keep Out Zone 2") if len(normal_vector_koz_array) > 1 else None
         ax1.plot(circle_points_array[2][:, 0], circle_points_array[2][:, 1], circle_points_array[2][:, 2], "purple", linewidth=2, label="Keep Out Zone 3") if len(normal_vector_koz_array) > 2 else None
     
