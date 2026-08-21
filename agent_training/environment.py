324a37abd5a22022dac1a24b9dc5a6d844509e35
Cemal Yilmaz <Cemal.Yilmaz@telematik-zentrum.de>
2026-08-21 15:46:49 +0200

    Updated reward functions.

--- agent_training/environment.py
+++ agent_training/environment.py
@@ -1339,6 +1339,65 @@ def reward_function(state, _q0_prev, torque, torque_prev, phase, state_koz, koz_
         
         r_total = r1 + r2 + r4 + r5
 
+    if USE_REWARD == "mod224ph2b":
+        """
+        Goal: use mod224 from phase 1 as baseline for phase 2 tuning
+        Result: 
+        Note: changed pointing acc to 0.25. +/- delta err same weight, - delta less weight if close to KOZ. pointing bonus even when violated.
+        """
+
+        # Reward for reducing attitude error
+        r1 = 0 
+        # Phase 1
+        if phase == "phase 1":
+            r1 = err_phi_delta
+            
+
+        # Phase 2
+        else:      
+            if koz_margin_min > 0.17:
+                r1 = err_phi_delta
+            elif koz_margin_min > 0:
+                r1 = err_phi_delta * (koz_margin_min/0.17)
+            else:
+                r1 = 0
+            
+
+        # Bonus for high accuracy
+        r2 = 0.0
+        if phase == "phase 1":
+            # Bonus for desired accuracy
+            if err_phi_current < 0.25:
+                r2 = 0.02
+            else:
+                r2 = 0.02 * np.exp((-err_phi_current + 0.2) * 1.0)
+
+        elif phase == "phase 2":
+            if err_phi_current < 0.25:
+                r2 = 0.02
+            else:
+                r2 = 0.02 * np.exp((-err_phi_current + 0.2) * 1.0)
+
+
+        # Penalty for using large torques
+        r4 = - 1.0*(abs(torque_1)+abs(torque_2)+abs(torque_3))
+
+        # Penalty for entering / being close to keep out zone
+        r5 = 0.0
+        if phase == "phase 2":
+            # Maximum penalty inside of KOZ
+            if koz_margin_min <= 0.0:
+                r5 = -1.0
+            # Gradial penalty starting at 0.17 rad or 9.7 deg margin
+            elif koz_margin_min < 0.17:
+                r5 = -1.0 * (1.0 - koz_margin_min/0.17)
+            # No penalty if farther away
+            else:
+                r5 = 0.0
+
+
+        r_total = r1 + r2 + r4 + r5
+
     if USE_REWARD == "mod23":
         """
         Goal: recover pointing accuracy in phase 2 (improve mod22).
